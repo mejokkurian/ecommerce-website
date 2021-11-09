db21b2bb9b1da51a5124c857888c9f89863f351f
mejokkurian <mejokkurian06@gmail.com>
2021-11-09 12:46:47 +0530

    finals

--- project/settings.py
+++ project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ri9c8!p_%$#wko6xj3rp&p@w^jgt@_5fapsgr=vge4u8uo9)v0'
+SECRET_KEY = config('sceret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('debug', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
